1696251e2f77c6b4db617b8bcd6d0b139f1467e6
Brian Slaby <brian.slaby@gmail.com>
2024-01-25 21:16:05 -0500

    fixed Uncaught TypeError: reduce of empty array with no initial value

--- src/components/ObligationChart.jsx
+++ src/components/ObligationChart.jsx
@@ -4,42 +4,46 @@ export default function ObligationChart({ characters }) {
     const [ charOblRanges, setCharOblRanges ] = useState(calculateObligationRanges(characters))
 
     function calculateObligationRanges(characters) {
-        let currentEndValue = 0
+        if (characters.length > 0) {
+            let currentEndValue = 0
 
-        const charactersArray = [...characters]
-        const newArray = charactersArray.map(character => {
-            const characterObligations = Object.entries(character).filter(entry => entry[0] !== "name")
-
-            const updatedObligations = characterObligations.map(obligation => {
-                const keyName = obligation[0]
-                const obligationName = obligation[1].name
-                const obligationValue = obligation[1].value
-                const newStartValue = currentEndValue + 1
-                const newEndValue = currentEndValue + parseInt(obligationValue)
-
-                currentEndValue = newEndValue
-
-                return {
-                    [keyName]: {
-                        "name": obligationName,
-                        "range": `${newStartValue.toString()} - ${newEndValue.toString()}`
+            const charactersArray = [...characters]
+            const newArray = charactersArray.map(character => {
+                const characterObligations = Object.entries(character).filter(entry => entry[0] !== "name")
+    
+                const updatedObligations = characterObligations.map(obligation => {
+                    const keyName = obligation[0]
+                    const obligationName = obligation[1].name
+                    const obligationValue = obligation[1].value
+                    const newStartValue = currentEndValue + 1
+                    const newEndValue = currentEndValue + parseInt(obligationValue)
+    
+                    currentEndValue = newEndValue
+    
+                    return {
+                        [keyName]: {
+                            "name": obligationName,
+                            "range": `${newStartValue.toString()} - ${newEndValue.toString()}`
+                        }
                     }
+                }).reduce((resultObj, currentObj) => {
+                    const key = Object.keys(currentObj)[0]
+                    resultObj[key] = currentObj[key]
+    
+                    return resultObj
+                }, {})
+    
+                const newCharObj = {
+                    "name": character.name,
+                    ...updatedObligations
                 }
-            }).reduce((resultObj, currentObj) => {
-                const key = Object.keys(currentObj)[0]
-                resultObj[key] = currentObj[key]
-
-                return resultObj
+    
+                return newCharObj
             })
-
-            const newCharObj = {
-                "name": character.name,
-                ...updatedObligations
-            }
-
-            return newCharObj
-        })
-        return newArray
+            return newArray
+        } else {
+            return []
+        }
     }
 
     useEffect(() => {
